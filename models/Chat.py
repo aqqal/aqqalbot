5673b8113b55852e92fae06c349735ff6d8bbc46
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-18 14:17:54 +0300

    updated Chat model

--- models/Chat.py
+++ models/Chat.py
@@ -2,5 +2,17 @@ from pydantic import BaseModel
 from typing import Optional, List
 from models.Message import Message
 
+
 class Chat(BaseModel):
+	id: str
+	started_timestamp: int
+	last_message_timestamp: int
+	bot_version_id: str
 	messages: List[Message]
+
+
+class BotVersion(BaseModel):
+	id: str
+	name: str
+	timestamp: int
+	prompt: str
