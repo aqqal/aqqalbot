b00a5e4c70e00ec824381cddd8dcfbbc5d517229
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-22 19:47:57 +0300

    added /bots endpoint models

---
+++ app/routers/bots/models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
+from models.Chat import Bot
+
+class NewBotRequest(BaseModel):
+	name: Optional[str]
+	prompt: str
+	model_id: str
+
+class UpdateBotRequest(BaseModel):
+	name: Optional[str]
+	prompt: Optional[str]
+	model_id: Optional[str]
+
+class GetBotsResponse(BaseModel):
+	bots: list[Bot]
+
+class NewChatResponse(BaseModel):
+	chat_id: str
+
