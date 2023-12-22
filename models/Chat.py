e02d83f114ab7af4f4b48c4cc1a9cfc8d3eb0632
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-22 21:03:42 +0300

    rearranged class defs

--- models/Chat.py
+++ models/Chat.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional, List
-from models.Message import Message
 
-class Chat(BaseModel):
-	id: Optional[str]
-	created_at: int
-	last_message: int
-	bot_id: str
-	messages: Optional[List[Message]]
+
+class TokenUsage(BaseModel):
+	completion_tokens: Optional[int]
+	prompt_tokens: Optional[int]
+	tokens: int
 
 
 class Message(BaseModel):
@@ -17,9 +15,11 @@ class Message(BaseModel):
 	content: str
 	context: Optional[str]
 	token_usage: Optional[TokenUsage]
+	
 
-
-class TokenUsage(BaseModel):
-	completion_tokens: Optional[int]
-	prompt_tokens: Optional[int]
-	tokens: int
+class Chat(BaseModel):
+	id: Optional[str]
+	created_at: int
+	last_message: int
+	bot_id: str
+	messages: Optional[List[Message]]
