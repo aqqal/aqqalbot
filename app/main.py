9fcab0037c98b618d45a0f9545051ff456f04ead
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-26 11:20:04 +0300

    mounted /chats

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.auth import validate_token
-# from app.routers.chats.router import router as chats_router
+from app.routers.chats.router import router as chats_router
 from app.routers.bots.router import router as bots_router
 
 from dotenv import load_dotenv
@@ -12,5 +12,5 @@ app = FastAPI(
     description="API service for Aqqal chatbot, Aqqalbot",
 )
 
-# app.include_router(chats_router)
+app.include_router(chats_router)
 app.include_router(bots_router)
