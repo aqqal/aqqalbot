48a68e623fcc2ee588c0fc5607d8308b00e31cea
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-29 18:42:16 +0300

    added default id for mongo

--- models/Chat.py
+++ models/Chat.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, List, Union
 from typing_extensions import Literal
 
 from models.bot import Bot
-from models.openai import OpenAIContext
-
+from models.openai_context import OpenAIContext
+import uuid
 
 class TokenUsage(BaseModel):
 	completion_tokens: Optional[int]
@@ -16,14 +16,17 @@ class Message(BaseModel):
 	created_at: int
 	by: str
 	content: str
-	token_usage: Optional[TokenUsage] = None
+	token_usage: Optional[TokenUsage]
 	
 
 class Chat(BaseModel):
-	id: Optional[str]
+	class Config:
+		allow_population_by_field_name = True
+
+	id: str = Field(default_factory=uuid.uuid4, alias="_id")
 	bot: Bot
 	created_at: int
 	last_message: int
 	messages: List[Message]
-	model_context: Optional[OpenAIContext] = []
+	model_context: Optional[OpenAIContext]
 
--- models/bot.py
+++ models/bot.py
@@ -1,8 +1,12 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+import uuid
+
 
 class Bot(BaseModel):
-	id: Optional[str]
+	class Config:
+		allow_population_by_field_name = True
+		
+	id: str = Field(default_factory=uuid.uuid4, alias="_id")
 	name: str
 	created_at: int
 	prompt: str
