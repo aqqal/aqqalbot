0ebda5e82dabbd87653bd89b2d54cc75bbbf1fc8
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-29 18:45:11 +0300

    bug fixes

--- bot/openai_completions.py
+++ bot/openai_completions.py
@@ -1,7 +1,7 @@
-from config import client
 
 from models.chat import Chat, Message
 from models.bot import Bot
+from models.openai_context import OpenAIContext
 
 from bot.config import client
 from bot.tools import get_available_tools, call_tools_on_chat
@@ -14,20 +14,45 @@ async def add_init_model_context_to_chat(chat: Chat) -> Chat:
 	
 	"""
 
-
 	if chat.model_context:
 		raise Exception("Model context already exists")
 
-	
-	chat.model_context.messages = [
-		{
-			"role": "system",
-			"content": chat.bot.prompt,
-		}
-	]
+	context = {
+		'messages': [
+			{
+				'role': 'system',
+				'content': chat.bot.prompt
+			}
+		]
+	}
 
+	chat.model_context = OpenAIContext(**context)
 	return chat
 	
+
+async def add_user_message_to_Chat(chat: Chat, content: str) -> Chat:
+	"""
+	Appends a user Message to chat object.
+	"""
+
+	message = Message(
+		content=content,
+		by="user",
+		created_at=int(time.time()),
+	)
+
+	chat.last_message = message.created_at
+
+	if chat.messages is None or chat.model_context is None:
+		raise Exception("Chat object not initialized")
+
+	chat.messages.append(message)
+	chat.model_context.messages.append({
+		"role": "user",
+		"content": content,
+	})
+
+	return chat
 
 async def add_bot_response_to_Chat(chat: Chat, use_tools=False) -> Chat:
 	"""
@@ -52,6 +77,7 @@ async def add_bot_response_to_Chat(chat: Chat, use_tools=False) -> Chat:
 		if not content:
 			raise Exception("No content returned from OpenAI")
 
+		timestamp = int(time.time())
 		message = Message(
 			content=content,
 			by="bot",
@@ -61,7 +87,7 @@ async def add_bot_response_to_Chat(chat: Chat, use_tools=False) -> Chat:
 		chat.last_message = timestamp
 		chat.messages.append(message)
 
-		return chat
+		return chat, message
 
 
 	chat = await call_tools_on_chat(chat, tool_calls)
