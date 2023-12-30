8be893dc8e2c83b52723d5d11ecb7b97b41e708c
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-30 17:09:40 +0300

    updated models

--- models/Chat.py
+++ models/Chat.py
@@ -6,6 +6,8 @@ from models.bot import Bot
 from models.openai_context import OpenAIContext
 import uuid
 
+import time
+
 class TokenUsage(BaseModel):
 	completion_tokens: Optional[int]
 	prompt_tokens: Optional[int]
@@ -13,10 +15,9 @@ class TokenUsage(BaseModel):
 
 
 class Message(BaseModel):
-	created_at: int
-	by: str
+	created_at: int = Field(default_factory=lambda:int(time.time()))
+	by: Literal["user", "bot"]
 	content: str
-	token_usage: Optional[TokenUsage]
 	
 
 class Chat(BaseModel):
@@ -25,8 +26,7 @@ class Chat(BaseModel):
 
 	id: str = Field(default_factory=uuid.uuid4, alias="_id")
 	bot: Bot
-	created_at: int
-	last_message: int
-	messages: List[Message]
-	model_context: Optional[OpenAIContext]
-
+	created_at: int = Field(default_factory=lambda:int(time.time()))
+	last_message: int = 0
+	messages: List[Message] = []
+	model_context: Optional[OpenAIContext] = None
