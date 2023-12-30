ec6d566e22d915df386c8bb42fbfe15dca7c0465
Hazem Elsayed <hazemm.els@gmail.com>
2023-12-30 17:08:16 +0300

    added main working imp

--- bot/openai_completions.py
+++ bot/openai_completions.py
@@ -5,6 +5,7 @@ from models.openai_context import OpenAIContext
 
 from bot.config import client
 from bot.tools import get_available_tools, call_tools_on_chat
+from bot.logger import logger
 
 import time
 
@@ -42,7 +43,7 @@ async def add_user_message_to_chat(chat: Chat, content: str) -> Chat:
 
 	chat.last_message = message.created_at
 
-	if not chat.messages or not chat.model_context:
+	if not chat.model_context:
 		raise Exception("Chat object not initialized")
 
 	chat.messages.append(message)
@@ -58,21 +59,20 @@ async def add_bot_response_to_chat(chat: Chat, use_tools=False) -> Chat:
 	Uses bot in chat object to append a response Message using OpenAI.
 	"""
 
-	tools = get_available_tools() if use_tools else None
-
+	logger.info(f"Calling OpenAI")
 	openai_response = await client.chat.completions.create(
 		messages=chat.model_context.messages,
 		model=chat.bot.model_id,
-		tools=tools,
 	)
-
-	openai_message = openai_response.choices[0]["message"]
+	logger.info(f"Received OpenAI response: {openai_response}")
+	
+	openai_message = openai_response.choices[0].message
 	chat.model_context.messages.append(openai_message)
 	
-	content = openai_message.get("content")
-	tool_calls = openai_message.get("tool_calls")
+	content = openai_message.content
+	tool_calls = openai_message.tool_calls
 	
-	if not tools or not tool_calls:
+	if not use_tools or not tool_calls:
 		if not content:
 			raise Exception("No content returned from OpenAI")
 
