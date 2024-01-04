8bd56d3817c2909e6795c2fdd65c915777a7f8aa
Hazem Elsayed <hazemm.els@gmail.com>
2024-01-04 18:19:03 +0300

    defined model_id values

--- models/bot.py
+++ models/bot.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Literal
 import uuid
 
 
@@ -10,4 +11,4 @@ class Bot(BaseModel):
 	name: str
 	created_at: int
 	prompt: str
-	model_id: str
+	model_id: Literal['gpt-3.5-turbo-1106', 'gpt-4-1106-preview']
